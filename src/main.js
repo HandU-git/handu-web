93f3ff49d8abafc701c9cf53ec6ed5b1a817b862
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2022-10-31 23:58:54 +0900

    vuex 셋업

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { router } from './routes/index.js'
+import { store } from './store/index.js'
 
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   vuetify,
-  router
+  router,
+  store,
 }).$mount('#app')
