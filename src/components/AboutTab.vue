0f2188a403c88735fc40594bde233f83a52395c8
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2022-11-10 15:02:22 +0900

    About Tab 완성

--- src/components/AboutTab.vue
+++ src/components/AboutTab.vue
@@ -2,12 +2,23 @@
   <div class="user-wrap">
     <div class="head">ABOUT</div>
     <div class="catch">핸듀만의 차별화된 기술, 당신만을 위한 서비스</div>
-    <div class="text1"></div>
-    <div class="text2"></div>
     <div class="container1">
+      <img class="item-img1" src="../assets/아이템_OCR.png" alt="" />
+      <div class="item-title1">OCR 손글씨 자동교정</div>
+      <div class="item-body1">
+        핸듀만의 기술로 높은 인식률을 자랑하는 OCR. <br />
+        손글씨 자동 교정을 체험해 보세요.
+      </div>
       <button @click="toDemoPage" class="btn">체험하기</button>
     </div>
-    <div  class="container2">
+    <div class="container2">
+      <img
+        class="item-img2"
+        src="../assets/아이템_킨더룸.png"
+        alt=""
+      />
+      <div class="item-title2">핸듀 포트폴리오</div>
+      <div class="item-body2">핸듀의 포트폴리오를 확인해보세요.</div>
       <button @click="toPortfolio" class="btn">자세하게</button>
     </div>
   </div>
@@ -34,19 +45,10 @@ export default {
   background-color: #e7e6e6;
 }
 
-.text1 {
-  position: absolute;
-}
-
-.text2 {
-  position: absolute;
-}
-
 .container1 {
   position: absolute;
   top: 250px;
-  left: 0%;
-  margin-left: 12vw;
+  right: 53.5vw;
   width: 500px;
   height: 374px;
   background-color: #fdfafa;
@@ -57,7 +59,7 @@ export default {
   position: absolute;
   top: 250px;
   height: 374px;
-  left: 55vw;
+  left: 53.5vw;
   width: 500px;
   border-radius: 6px;
   background-color: #fdfafa;
@@ -73,6 +75,7 @@ export default {
   color: #fffbfb;
   background-color: #8195c9;
   width: 135px;
+  text-align: center;
   height: 45px;
 }
 
@@ -91,5 +94,53 @@ export default {
   left: 37vw;
   font-size: 20px;
   font-weight: 400;
+}
+
+.item-img1 {
+  top: 46px;
+  left: 33%;
+  position: absolute;
+  width: 160px;
+}
+
+.item-img2 {
+  position: absolute;
+  top: 39px;
+  left: 21%;
+  width: 297px;
+}
+
+.item-title1 {
+  position: relative;
+  text-align: center;
+  top: 190px;
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.item-title2 {
+  position: relative;
+  text-align: center;
+  top: 190px;
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.item-body1 {
+  position: relative;
+  text-align: center;
+  top: 200px;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 29px;
+}
+
+.item-body2 {
+  position: relative;
+  text-align: center;
+  top: 215px;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 29px;
 }
 </style>
