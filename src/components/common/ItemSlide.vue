ea3888bf2d75a74844bf35747f3118fe65b5215d
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2022-11-16 16:31:24 +0900

    포트폴리오 슬라이드  컴포넌트 템플릿 title, tag, body 완료

---
+++ src/components/common/ItemSlide.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="slide-container">
+    <div class="slide-item">
+      <div class="slide-title">
+        {{ leftItem.title }}
+      </div>
+      <div class="slide-tags-container">
+        <div
+          class="slide-tag"
+          v-for="(tag, index) in leftItem.tags"
+          :key="index"
+        >
+          <img
+            class="tag-image"
+            :src="require(`../../assets/${tag}`)"
+            alt="플랫폼 태그"
+          />
+        </div>
+      </div>
+      <div class="slide-body">
+        <slot name="left-body">dafault body</slot>
+      </div>
+      <img class="slide-main-image" src="" alt="메인사진" />
+    </div>
+    <div class="slide-item">
+      <div class="slide-title">
+        {{ rightItem.title }}
+      </div>
+      <div class="slide-tags-container">
+        <div
+          class="slide-tag"
+          v-for="(tag, index) in rightItem.tags"
+          :key="index"
+        >
+          <img
+            class="tag-image"
+            :src="require(`../../assets/${tag}`)"
+            alt="플랫폼 태그"
+          />
+        </div>
+      </div>
+      <div class="slide-body">
+        <slot name="right-body">dafault body</slot>
+      </div>
+      <img class="slide-main-image" src="" alt="메인사진" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    leftItem: Object,
+    rightItem: Object,
+  },
+};
+</script>
+
+<style scoped>
+.slide-item {
+  position: relative;
+  width: 47vw;
+  height: 430px;
+  margin: 1.5vw;
+  background-color: #ffffff;
+}
+.slide-title {
+  padding: 100px 0px 0px 43px;
+  font-size: 24px;
+  font-weight: 900;
+}
+.slide-tags-container {
+  padding: 10px 0px 0px 38px;
+  display: flex;
+}
+
+.slide-tag {
+  padding-right: 12px;
+}
+
+.tag-image {
+  width: 81px;
+}
+
+.slide-body {
+  padding: 0 0 10px 43px;
+  font-size: 20px;
+  font-weight: 500;
+  color: #787474;
+  line-height: 39px;
+}
+
+.slide-main-image {
+    position: absolute;
+    left: 50%;
+    top: 96px;
+}
+</style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,49 +1,69 @@
 <template>
-  <div>
-    <div style="text-align: center;">
-      <h1>
-        포트폴리오
-      </h1>
+  <div style="background-color: #e7e5e5">
+    <div style="text-align: center">
+      <h1>포트폴리오</h1>
     </div>
-    <div>
-      <div style="display: flex;">
+    <div style="align-items: center">
+      <ItemSlide :leftItem="obj1" :rightItem="obj2">
+        <span slot="left-body">
+          <div>글자 내 오탈자 인식</div>
+          <div>오타 위치 및 교정 가이드 제공</div>
+          <div>한국어, 영어 인식 학습 모델 제공</div>
+          <div>일본어, 중국어 등 다국어 확장 예정</div>
+        </span>
+      </ItemSlide>
+      <div class="slide-container">
         <div class="left-item">
-          AI 손 글씨 자동 교정
+          <div class="slide-title">학습 관리자 서비스</div>
         </div>
-        <div class="right-item">
-          AI 자동 첨삭 서비스
-        </div>
+        <div class="right-item">자동 채점 서비스</div>
       </div>
-      <div style="display: flex;">
-        <div class="left-item">
-          학습 관리자 서비스
-        </div>
-        <div class="right-item">
-          자동 채점 서비스
-        </div>
-      </div>
-      <div style="text-align: center">
-        <div class="left-item">
-          다국어 학습 단어장
-        </div>
-        <div class="right-item">
-          자동 채점 서비스
-        </div>
+      <div class="slide-container">
+        <div class="left-item">다국어 학습 단어장</div>
       </div>
     </div>
   </div>
 </template>
 
+<script>
+import ItemSlide from "@/components/common/ItemSlide.vue";
+
+export default {
+  data() {
+    return {
+      obj1: {
+        title: "AI 손글씨 자동 교정",
+        tags: ["mobiletag.png", "tablettag.png"],
+        body: [
+          "글자 내 오탈자 인식",
+          "오타 위치 및 교정 가이드 제공",
+          "한국어, 영어 인식 학습 모델 제공",
+          "일본어, 중국어 등 다국어 확장 예정",
+        ],
+      },
+      obj2: {
+        title: "AI 자동 첨삭 서비스",
+        tags: ["mobiletag.png", "tablettag.png"],
+        body: [
+          "태블릿 내 단어 첨삭 서비스 제공",
+          "기존 학습 콘텐츠에 쓰기 콘텐츠 추가",
+          "피드백 시간 단축 (3일 -> 30분)",
+          "맞춤법, 스펠링 오류 혼자 파악 가능",
+        ],
+      },
+    };
+  },
+
+  components: {
+    ItemSlide,
+  },
+};
+</script>
 
 <style scoped>
-.left-item {
-
+.slide-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
-
-.right-item {
-
-  
-}
-
-
 </style>
